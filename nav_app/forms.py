09a08858f95b00b2dec84327ba4445073d4d1390
--replace-all <promit666@gmail.com>
2023-03-20 19:34:48 +0600

    Remove html forms files

--- nav_app/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import GetUserInfo
-
-
-class GetUserInfoForm(forms.ModelForm):
-    class Meta:
-        model = GetUserInfo
-        fields = ['location', 'departure_time']
