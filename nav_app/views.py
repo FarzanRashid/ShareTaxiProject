3dcaf624b12ca5614907483ffe3ed1084ac01238
--replace-all <promit666@gmail.com>
2023-03-23 11:49:39 +0600

    Connect django app with postgres

--- nav_app/views.py
+++ nav_app/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from .models import GetUserInfo
 
 
+@csrf_exempt
 def book_taxi(request):
-    if request.method == "POST":
-        form = GetUserInfoForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return render(request, 'nav_app/success.html')
+    if request.method == 'POST':
+        json_data = request.body.decode('utf-8')
+        data = json.loads(json_data)
+        my_model = GetUserInfo(location=data['location'], departure_time=data['departure_time'], email=data['email'])
+        my_model.save()
+        response_data = {'success': True}
+        return JsonResponse(response_data)
     else:
-        form = GetUserInfoForm()
-    return render(request, 'nav_app/nav_app.html', {'form': form})
+        response_data = {'error': 'Invalid request method'}
+        return JsonResponse(response_data, status=400)
