2b4623bb499ebb7d6a16614e937b475479c65b42
--replace-all <promit666@gmail.com>
2023-04-06 13:04:23 +0600

    Adding tests for nav_app

--- nav_app/tests.py
+++ nav_app/tests.py
@@ -9,18 +9,22 @@ class BookTaxiTestCase(TestCase):
     def test_book_taxi_success(self):
         url = reverse('book_taxi')
         data = {
-            'location': 'Azimpur',
+            'dropoff_location': 'Azimpur',
             'departure_time': '10:00',
-            'email': 'test@example.com'
+            'email': 'test@example.com',
+            'pickup_location': 'DMD',
+            'date': "2023-04-03"
         }
         response = self.client.post(url, json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(RideRequest.objects.count(), 1)
-        self.assertEqual(RideRequest.objects.first().location, 'Azimpur')
+        self.assertEqual(RideRequest.objects.first().dropoff_location, 'Azimpur')
+        self.assertEqual(RideRequest.objects.first().pickup_location, 'DMD')
         departure_time = RideRequest.objects.first().departure_time
         expected_departure_time = datetime.strptime('10:00', '%H:%M').time()
         self.assertEqual(departure_time, expected_departure_time)
         self.assertEqual(RideRequest.objects.first().email, 'test@example.com')
+        self.assertEqual(RideRequest.objects.first().date, datetime.strptime('2023-04-03', '%Y-%m-%d').date())
 
     def test_book_taxi_invalid_method(self):
         url = reverse('book_taxi')
@@ -31,9 +35,11 @@ class BookTaxiTestCase(TestCase):
     def test_book_taxi_invalid_data(self):
         url = reverse('book_taxi')
         data = {
-            'location': 'DMD',
+            'dropoff_location': 'DMD',
             'departure_time': 'invalid',
-            'email': 'test@example.com'
+            'email': 'test@example.com',
+            'pickup_location': 'Azimpur',
+            'date': "2023-04-03"
         }
         response = self.client.post(url, json.dumps(data), content_type='application/json')
         self.assertEqual(response.status_code, 400)
